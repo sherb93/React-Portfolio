c406e6addc7abd6dfca83f59a1bec10ce7966068
Robert Slocumbe <rslocumbe93@gmail.com>
2022-06-15 11:29:19 -0400

    page component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import AboutMe from './AboutMe';
+import Contact from './Contact';
+import Projects from './Projects';
+import Resume from './Resume';
 
-function Page() {
+function Page({ currentPage }) {
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case "About Me":
+                return <AboutMe />;
+            case "Projects":
+                return <Projects />;
+            case "Resume":
+                return <Resume />;
+            case "Contact Me":
+                return <Contact />;
+            default:
+                return <AboutMe />
+        }
+    }
+
     return (
-        <>
-        </>
+        <section>
+            {renderPage()}
+        </section>
     );
 }
 

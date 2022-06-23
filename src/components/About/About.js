d8d3c686afbb1c975091c3791cfd5a6d49197085
Robert Slocumbe <rslocumbe93@gmail.com>
2022-06-23 17:52:11 -0400

    about me section layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { AiOutlineUser } from 'react-icons/ai'
+import Selfie from '../../assets/portrait.png'
 import './about.css';
 
 function About() {
     return (
-        <h1>About</h1>
+        <section id="about">
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={Selfie} alt="Robert's handsome face" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <AiOutlineUser className="about__icon" />
+                            <h5>Experience</h5>
+                            <small>3 Years</small>
+                        </article>
+                        <article className="about__card">
+                            <AiOutlineUser className="about__icon" />
+                            <h5>Experience</h5>
+                            <small>3 Years</small>
+                        </article>
+                        <article className="about__card">
+                            <AiOutlineUser className="about__icon" />
+                            <h5>Experience</h5>
+                            <small>3 Years</small>
+                        </article>
+                    </div>
+
+                    <p>
+                        Hi, I'm Robert and this is where I'm writing about myself.
+                        Hi, I'm Robert and this is where I'm writing about myself.
+                        Hi, I'm Robert and this is where I'm writing about myself.
+                        Hi, I'm Robert and this is where I'm writing about myself.
+                        Hi, I'm Robert and this is where I'm writing about myself.
+                    </p>
+
+                    <a href="contact" className="btn btn-primary">Let's Talk</a>
+                </div>
+            </div>
+        </section>
     );
 }
 
